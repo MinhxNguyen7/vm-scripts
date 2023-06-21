f921178e4e2ea5c04987b927a34c0f85a3025a58
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-21 16:10:48 -0700

    Create ubuntu-setup.sh

---
+++ ubuntu-setup.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Update apt-get
+sudo apt-get update
+
+# Get tools
+sudo apt-get install wget gpg
+
+# Install Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install apt-transport-https
+sudo apt-get install sublime-text
+
+# Install VSCode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install code
+
+# Install Jetbrains Toolbox
+wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+tar -xzf jetbrains-toolbox.tar.gz
+DIR=$(find . -maxdepth 1 -type d -name jetbrains-toolbox-\* -print | head -n1)
+cd $DIR
+./jetbrains-toolbox
+cd ..
+rm -r $DIR
+rm jetbrains-toolbox.tar.gz
+
+# SSH
+sudo apt install openssh-server
