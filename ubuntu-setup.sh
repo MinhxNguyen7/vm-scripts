79ee2b75e762d16cc5579071b7829eff4b8bed76
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-21 16:23:25 -0700

    Update sublime source

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install wget gpg
 
 # Install Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get install apt-transport-https
 sudo apt-get install sublime-text
 
