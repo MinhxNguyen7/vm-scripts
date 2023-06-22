70cddd4140a7d8f4b6a815ffa054f19878da030e
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-21 17:13:34 -0700

    NFS

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -35,3 +35,17 @@ sudo apt install -y openssh-server
 
 # Terminator
 sudo apt install -y terminator
+
+# NFS Setup
+sudo apt install -y nfs-common
+read -p "Enter NFS share IP (leave blank to skip): " NFS_IP
+if [ ! -z $NFS_IP ]; then # If not empty
+  read -p "Enter NFS share name (without slash): " NFS_NAME
+  read -p "Enter mount point: " NFS_MOUNT
+
+  sudo mkdir $NFS_MOUNT
+  echo "$NFS_IP:/$NFS_NAME $NFS_MOUNT nfs defaults 0 0" >> "/etc/fstab"
+  sudo mount $NFS_MOUNT
+fi
+
+
