a7b9f9d2688f103c0adf0cf3925f9530a86fac38
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-28 17:36:45 -0700

    Create tailscale.sh

---
+++ tailscale.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install curl
+
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt-get update
+sudo apt-get install tailscale
+
+sudo tailscale up
+
+rm /etc/apt/sources.list.d/tailscale.list
+rm /usr/share/keyrings/tailscale-archive-keyring.gpg
+printf %"$COLUMNS"s |tr " " "-"
