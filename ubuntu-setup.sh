c3007c8cceb19ae03404698d444c5443740a7a35
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-28 19:48:31 -0700

    Streamline VSCode and Code Tunnel

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -21,15 +21,6 @@ sudo apt-get install apt-transport-https
 sudo apt-get install sublime-text
 printf %"$COLUMNS"s |tr " " "-" # Horizontal line
 
-# Install VSCode
-echo "Installing VSCode"
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install -y code
-printf %"$COLUMNS"s |tr " " "-" # Horizontal line
-
 # SSH
 echo "Installing OpenSSH Server"
 sudo apt install -y openssh-server
@@ -75,4 +66,10 @@ while true; do
     echo "Invalid input. Valid inputs: Y for yes, n for no"
     fi
 done
+printf %"$COLUMNS"s |tr " " "-" # Horizontal line
+
+# Install VSCode
+echo "Installing VSCode"
+sudo snap install code --classic
+code tunnel service install --accept-server-license-terms --disable-telemetry
 printf %"$COLUMNS"s |tr " " "-" # Horizontal line
