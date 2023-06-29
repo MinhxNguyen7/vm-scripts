59bd5546805eb269ad878403c84c0d6287e71dc2
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-06-28 17:55:11 -0700

    yes

--- tailscale.sh
+++ tailscale.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt install curl
+sudo apt install -y curl
 
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
 sudo apt-get update
-sudo apt-get install tailscale
+sudo apt-get -y install tailscale
 
 sudo tailscale up
 
