4aa0ea9fca73eefec94ba196c5f6cd603a9af463
MinhxNguyen7 <mxnguye1@uci.edu>
2023-10-09 18:19:01 -0700

    Maybe fix conditional

--- code-server-setup.sh
+++ code-server-setup.sh
@@ -21,25 +21,25 @@ sudo bash nfs-mount.sh
 read -p "Install TailScale? (Y/n): " INSTALL_TAILSCALE
 echo ""
 if [[ "$INSTALL_TAILSCALE" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        curl -fsSL https://tailscale.com/install.sh | sh
-        sudo tailscale up # Start TailScale
-        printf %"$COLUMNS"s |tr " " "-" # Horizontal line
-    else
-        echo "Skipping TailScale installation"
+then
+    curl -fsSL https://tailscale.com/install.sh | sh
+    sudo tailscale up # Start TailScale
+    printf %"$COLUMNS"s |tr " " "-" # Horizontal line
+else
+    echo "Skipping TailScale installation"
 fi
 
 # Web development tools optional
 read -p "Install web development tools (NodeJS, NPM, pnpm)? (Y/n): " INSTALL_WEB
 echo ""
 if [[ "$INSTALL_WEB" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        sudo apt install nodejs npm
-        sudo npm i -g pnpm n
-        sudo n stable
-        printf %"$COLUMNS"s |tr " " "-" # Horizontal line
-    else
-        echo "Skipping web development tools installation"
+then
+    sudo apt install nodejs npm
+    sudo npm i -g pnpm n
+    sudo n stable
+    printf %"$COLUMNS"s |tr " " "-" # Horizontal line
+else
+    echo "Skipping web development tools installation"
 fi
 
 # Install VSCode
